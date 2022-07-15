7187755c49810814002945dc2a7ac033cc8fc12f
Kevin Fortier <kevin.r.fortier@gmail.com>
2022-07-15 17:04:21 -0400

    feat: add amplify auth

--- objective-ai-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ objective-ai-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "objectiveaiappaa3bfe0b": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}

3fe69488f08fe5f0c0382724daff1afda6b74aba
Kevin Fortier <kevin.r.fortier@gmail.com>
2022-07-16 22:17:19 -0400

    feat: start on user flow ui

--- objective-ai-app/pages/_app.js
+++ objective-ai-app/pages/_app.js
@@ -1,6 +1,15 @@
+import "bootstrap/dist/css/bootstrap.css";
+
 import '../styles/globals.css'
 
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    import('bootstrap')
+  }, [])
   return <Component {...pageProps} />
 }
 
---
+++ objective-ai-app/pages/_init.js
@@ -0,0 +1,14 @@
+
+import { Button } from 'bootstrap';
+
+Amplify.configure(awsExports);
+
+function InitializeApp() {
+  return (
+    <div className={styles.container}>
+
+    </div>
+    );
+}
+
+export default InitializeApp;
